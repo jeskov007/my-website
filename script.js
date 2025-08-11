4135563f89d219abec22e416d7933f2259ea7d20
jeskov007 <jeskov007@gmail.com>
2025-08-11 11:48:52 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,63 @@
+// Array to store projects
+let projects = [
+    {
+        name: "DRAWER",
+        description: "Drawer is one of my first projects I made, it is a drawing app. How to use click to erase, and release to draw.(mouse needed)",
+        link: "https://editor.p5js.org/Jes007kov/sketches/LxPMhbcVx"
+    },
+    {
+        name: "Bouncing Link",
+        description: "This is a satisfying app. You watch link bounce around hitting walls and change tint. How to use up and down arrows.",
+        link: "https://editor.p5js.org/Jes007kov/sketches/e73kBIKQg"
+    },
+    {
+        name: "Random Number",
+        description: "This app randomizes a number between 1-1000. Just click the screen and the number will change.",
+        link: "https://editor.p5js.org/Jes007kov/sketches/i8R5Oj-hv"
+    },
+   {
+        name: "DRAWER",
+        description: "Drawer is one of my first projects I made, it is a drawing app. How to use click to erase, and release to draw.(mouse needed)",
+        link: "https://editor.p5js.org/Jes007kov/sketches/LxPMhbcVx"
+    },
+    {
+        name: "Bouncing Link",
+        description: "This is a satisfying app. You watch link bounce around hitting walls and change tint. How to use up and down arrows.",
+        link: "https://editor.p5js.org/Jes007kov/sketches/e73kBIKQg"
+    },
+    {
+        name: "Random Number",
+        description: "This app randomizes a number between 1-1000. Just click the screen and the number will change.",
+        link: "https://editor.p5js.org/Jes007kov/sketches/i8R5Oj-hv"
+    },
+
+    // Add more projects here (copy and paste above and edit)
+];
+
+// Function to display projects
+function displayProjects() {
+    const projectContainer = document.getElementById("projectContainer");
+    projectContainer.innerHTML = ""; // Clear previous projects
+
+    projects.forEach((project, index) => {
+        const projectDiv = document.createElement("div");
+        projectDiv.className = "project";
+        projectDiv.innerHTML = `
+            <h2>${project.name}</h2>
+            <p>${project.description}</p>
+            ${project.link ? `<a href="${project.link}">View Project</a>` : ''}
+            <button onclick="deleteProject(${index})">Delete</button>
+        `;
+        projectContainer.appendChild(projectDiv);
+    });
+}
+
+function deleteProject(index) {
+    projects.splice(index, 1);
+    displayProjects();
+}
+
+// Initial display
+document.addEventListener("DOMContentLoaded", () => {
+    displayProjects();
+});
